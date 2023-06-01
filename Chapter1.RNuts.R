2915d0fa2bc239a536b8fedfc675ae34c99f39c4
azizemabg <azizemabg@gmail.com>
2023-06-01 21:36:53 +0200

    Initial commit for chapter 1. R Nuts and Bolts

---
+++ Chapter1.RNuts.R
@@ -0,0 +1,18 @@
+
+# R Nuts and Bolts ####
+
+# Entering input: This is very basic tutorial regarding how to store a value in variable
+# Example as below
+
+c <- 1
+text <- "Hello guys"
+# Character (#) is comment, this the code will not be executed
+
+# The result will be printed if you either type the name of variable or using print() function
+x
+print(c) # the result is the same with above
+
+c <- 11:30
+c
+# When an R vector is printed you will notice that an index for the vector is printed in square brackets
+
